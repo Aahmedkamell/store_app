90d62d5e179dce68c697b97620ae6518f2f44740
Aahmedkamell <aahmed5kamell@gmail.com>
2026-09-20 04:10:49 +0300

    Add categories services

---
+++ lib/services/categories_service.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:store_app/models/product_model.dart';
+
+class CategoriesService {
+  Future<List<ProductModel>> getCategoriesProducts({
+    required String categoryName,
+  }) async {
+    http.Response response = await http.get(
+      Uri.parse('https://dummyjson.com/products/category/$categoryName'),
+    );
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(response.body);
+
+      List<ProductModel> productList = [];
+
+      for (int i = 0; i < data['products'].length; i++) {
+        productList.add(ProductModel.fromJson(data['products'][i]));
+      }
+      return productList;
+    } else
+      throw Exception(
+        'There is a problem with status code ${response.statusCode}',
+      );
+  }
+}
--- lib/services/get_all_product_services.dart
+++ lib/services/get_all_product_services.dart
@@ -4,17 +4,22 @@ import 'package:store_app/models/product_model.dart';
 import 'package:http/http.dart' as http;
 
 class AllProductServices {
- Future< List<ProductModel>> getAllProducts() async {
+  Future<List<ProductModel>> getAllProducts() async {
     http.Response response = await http.get(
       Uri.parse('https://dummyjson.com/products'),
     );
 
-    Map<String,dynamic> data = jsonDecode(response.body);
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(response.body);
 
-    List<ProductModel> productsList = [];
-    for (int i = 0; i < data['products'].length; i++) {
-      productsList.add(ProductModel.fromJson(data['products'][i]));
-    }
-    return productsList;
+      List<ProductModel> productsList = [];
+      for (int i = 0; i < data['products'].length; i++) {
+        productsList.add(ProductModel.fromJson(data['products'][i]));
+      }
+      return productsList;
+    } else
+      throw Exception(
+        'There is a problem with status code${response.statusCode}',
+      );
   }
 }
