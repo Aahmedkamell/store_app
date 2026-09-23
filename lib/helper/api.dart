9e9683bf198810e84ef3e19110d8d8b4c21e9057
Aahmedkamell <aahmed5kamell@gmail.com>
2026-09-23 22:23:59 +0300

    Implement Post Api Integration

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
@@ -7,13 +8,30 @@ class Api {
     http.Response response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
-      return jsonDecode(response.body); 
+      return jsonDecode(response.body);
     } else {
       throw Exception(
         'There is a problem with status code ${response.statusCode}',
-
-
       );
     }
   }
+}
+
+Future<dynamic> post({
+  required String url,
+  required dynamic body,
+  String? token,
+}) async {
+  Map<String, String> headers = {};
+  if (token != null) {
+    headers.addAll({'Authorization': 'Bearer $token'});
+  }
+  http.Response response = await http.post(
+    Uri.parse(url),
+    body: body,
+    headers: headers,
+  );
+
+  Map<String, dynamic> data = jsonDecode(response.body);
+  return data;
 }
