2c8c8a498819c530250c3294d7c3bbdd3658ed2f
Aahmedkamell <aahmed5kamell@gmail.com>
2026-09-24 14:55:44 +0300

    implement add product service

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
@@ -15,23 +14,29 @@ class Api {
       );
     }
   }
-}
 
-Future<dynamic> post({
-  required String url,
-  required dynamic body,
-  String? token,
-}) async {
-  Map<String, String> headers = {};
-  if (token != null) {
-    headers.addAll({'Authorization': 'Bearer $token'});
+  Future<dynamic> post({
+    required String url,
+    required dynamic body,
+    String? token,
+  }) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    http.Response response = await http.post(
+      Uri.parse(url),
+      body: body,
+      headers: headers,
+    );
+
+    if (response.statusCode == 201) {
+      Map<String, dynamic> data = jsonDecode(response.body);
+      return data;
+    } else {
+      throw Exception(
+        'There is a problem with status code ${response.statusCode} with body ${jsonDecode(response.body)}',
+      );
+    }
   }
-  http.Response response = await http.post(
-    Uri.parse(url),
-    body: body,
-    headers: headers,
-  );
-
-  Map<String, dynamic> data = jsonDecode(response.body);
-  return data;
 }
